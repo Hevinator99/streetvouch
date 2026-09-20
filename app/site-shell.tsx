63f0f257a8f5a9d0ed4bfec57e22c69a7c663b2b
StreetVouch Builder <builder@streetvouch.local>
2026-09-20 12:40:47 +0100

    Refresh public logo styling

--- app/site-shell.tsx
+++ app/site-shell.tsx
@@ -1,5 +1,5 @@
 import Script from "next/script";
 
 export default function SiteShell() {
-  return <><link rel="stylesheet" href="/style.css" /><div id="app" /><Script src="/app.js" strategy="afterInteractive" /><Script src="/pilot.js" strategy="afterInteractive" /><Script src="/marketing-demo.js" strategy="afterInteractive" /></>;
+  return <><link rel="stylesheet" href="/style.css?v=straight-logo" /><div id="app" /><Script src="/app.js" strategy="afterInteractive" /><Script src="/pilot.js" strategy="afterInteractive" /><Script src="/marketing-demo.js" strategy="afterInteractive" /></>;
 }
